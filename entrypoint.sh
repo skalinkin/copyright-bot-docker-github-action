9ef5a819a686e466adcb315078837d428905f523
skalinkin <skalinkin@gmail.com>
2023-03-01 14:17:48 -0500

    main

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,4 @@ echo "Hello"
 pwd
 git branch -r
 git fetch origin
-DEFAULT_BRANCH=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
-git diff --name-only --diff-filter=ACMR $DEFAULT_BRANCH .
+git diff --name-only --diff-filter=ACMR origin/main .
